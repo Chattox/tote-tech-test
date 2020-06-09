55db55456690f19d10263759c05d5da57a544d6a
Chattox <conradlineker@gmail.com>
2020-06-09 21:41:54 +0100

    Created Article card view from hardcoded data

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,11 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import getTechNews from '../src/config/config';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  getTechNews();
 });
---
+++ src/components/Article.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {View, TouchableHighlight} from 'react-native';
+import {Text, Card, Divider} from 'react-native-elements';
+import moment from 'moment';
+
+const defaultJSONData = {
+  source: {
+    id: null,
+    name: 'Theguardian.comworld',
+  },
+  author: 'Aamna Mohdin',
+  title:
+    'Protesters rally in Oxford for removal of Cecil Rhodes statue - The Guardian',
+  description:
+    'Campaigners and Black Lives Matter protesters block road outside Oriel College',
+  url:
+    'https://amp.theguardian.comworld/2020/jun/09/protesters-rally-in-oxford-for-removal-of-cecil-rhodes-statue',
+  urlToImage:
+    'https://i.guim.co.uk/img/media/9da4c25baa3868d772f55c462970d521bb94aaf1/0_400_6000_3600/master/6000.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=025527c05cd167406426d891f3b234f0',
+  publishedAt: '2020-06-09T18:23:43Z',
+  content:
+    'More than a thousand protesters have gathered outside Oxford University to demand the removal of a statue of the Victorian imperialist Cecil Rhodes.\r\nBlocking the road outside Oriel College, the Rhod… [+4130 chars]',
+};
+
+class Article extends Component {
+  render() {
+    const time = moment(defaultJSONData.publishedAt || moment.now()).fromNow();
+
+    return (
+      <TouchableHighlight>
+        <Card
+          featuredTitle={defaultJSONData.title}
+          featuredTitleStyle={{
+            marginHorizontal: 5,
+            textShadowColor: '#00000f',
+            textShadowOffset: {width: 3, height: 3},
+            textShadowRadius: 3,
+          }}
+          image={{uri: defaultJSONData.urlToImage}}>
+          <Text style={{marginBottom: 10}}>
+            {defaultJSONData.description || 'Read more...'}
+          </Text>
+          <Divider style={{backgroundColor: '#dfe6e9'}} />
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text
+              style={{
+                margin: 5,
+                fontStyle: 'italic',
+                color: '#b2bec3',
+                fontSize: 10,
+              }}>
+              {defaultJSONData.source.name.toUpperCase()}
+            </Text>
+
+            <Text
+              style={{
+                margin: 5,
+                fontStyle: 'italic',
+                color: '#b2bec3',
+                fontSize: 10,
+              }}>
+              {time}
+            </Text>
+          </View>
+        </Card>
+      </TouchableHighlight>
+    );
+  }
+}
+
+export default Article;
