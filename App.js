be884ebced9266324ccd77bc5c3fc96bb6fba8f2
Chattox <conradlineker@gmail.com>
2020-06-10 13:55:56 +0100

    Cleaned up App.js

--- App.js
+++ App.js
@@ -7,12 +7,3 @@ export default class App extends Component {
     return <News />;
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#fff',
-//   },
-// });
